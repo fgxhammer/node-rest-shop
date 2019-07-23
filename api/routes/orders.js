381f38633f0762186f8329ee7609d27c13f039bb
Franz Gollhammer <franz.gollhammer@gmail.com>
2019-07-24 01:13:35 +0200

    populate orders api with product information

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,6 +10,7 @@ const PORT = 3000
 router.get('/', (req, res, next) => {
 	Order.find()
 		.select("product quantity _id")
+		.populate("product", "name")
 		.exec()
 		.then(docs => {
 			res.status(200).json({
@@ -38,6 +39,7 @@ router.get('/', (req, res, next) => {
 //Get order by id
 router.get('/:orderId', (req, res, next) => {
 	Order.findById(req.params.orderId)
+		.populate("product")
 		.exec()
 		.then(order => {
 			if(!order) {
